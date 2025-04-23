a7caef65a13de53e660cf8b0cae0e010c1532123
localmanplaysgames <efitton5491@gmail.com>
2025-04-23 09:07:41 +0100

    FPS added

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
 
         for event in pygame.event.get():
@@ -19,6 +22,8 @@ def main():
         screen.fill((0, 0, 0))
         
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
