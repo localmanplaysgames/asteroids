350b285f9a617a882673116464ea381845aade76
localmanplaysgames <efitton5491@gmail.com>
2025-04-25 11:24:32 +0100

    no more laser beams

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -25,6 +26,8 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        if self.shot_timer > 0:
+            self.shot_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt * -1)
@@ -35,7 +38,9 @@ class Player(CircleShape):
         if keys[pygame.K_w]:
             self.move(dt * 1)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.shot_timer <= 0:
+                self.shoot()
+                self.shot_timer = PLAYER_SHOOT_COOLDOWN
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
