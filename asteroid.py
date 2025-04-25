cffdfa513a5d3e61544af15697d23b15641021d6
localmanplaysgames <efitton5491@gmail.com>
2025-04-25 12:46:20 +0100

    asteroids coloured according to size

--- asteroid.py
+++ asteroid.py
@@ -9,7 +9,13 @@ class Asteroid(CircleShape):
         super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+        if self.radius == ASTEROID_MAX_RADIUS:
+            colour = "red"
+        elif self.radius == ASTEROID_MIN_RADIUS:
+            colour = "yellow"
+        else:
+            colour = "orange"
+        pygame.draw.circle(screen, colour, self.position, self.radius, 2)
 
     def update(self, dt):
         self.position += self.velocity * dt
