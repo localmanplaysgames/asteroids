57dbba31a7a36e22998f5830bb45a218be1da9fb
localmanplaysgames <efitton5491@gmail.com>
2025-08-12 20:50:07 +0100

    fps introduced

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
 
@@ -18,6 +20,8 @@ def main():
 
         screen.fill('black')
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == '__main__':
     main()
